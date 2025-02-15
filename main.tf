d07bda1e4180219f0db5ce0228b432213076fb01
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-15 19:18:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,4 +11,11 @@ resource "aws_s3_bucket" "terraform_state" {
     Environment = "Dev"
   }
 }
+terraform {
+  backend "s3" {
+    bucket         = my-terraform-state-bucket-sunil7756
+    key            = "backend/terraform.tfstate"
+    region         = "ap-south-1"
+  }
+}
 
